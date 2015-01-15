b3676bfbb01040d097567915083b93db134fa661
omagari.tomohisa <oomatomo@gmail.com>
2015-01-15 21:20:42 +0900

    change hello

--- hello.go
+++ hello.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println("hello world")
+	id := "hoge"
+	hogeID := "hoge"
+	fmt.Println("hello world")
+	fmt.Println(id)
+	fmt.Println(hogeID)
+	fmt.Println(Square(2))
+}
+
+// Square return the
+func Square(n int) int {
+	return n * n
 }
